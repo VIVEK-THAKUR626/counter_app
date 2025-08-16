f8af5ef64b26231e4621a27384cd97583e04777f
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2025-08-16 19:34:44 +0530

    fix the issue of increase in box size while increasing the font size of the counter

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 window.onload = function () {
     let counter = document.getElementById("number");
-    counter.value = 0;
+    counter.textContent = 0;
 
     let decrement = document.getElementById("subtract");
     let increment = document.getElementById("add");
     let reset = document.getElementById("zero");
 
     decrement.addEventListener("click",() => {
-        if(counter.value == 0){
-            counter.value = 0;
+        if(counter.textContent === 0){
+            counter.textContent = 0;
         }else{
-            counter.value -= 1;
+            counter.textContent -= 1;
         }
     })
     increment.addEventListener("click",() => {
-        counter.value = Number(counter.value) + 1;
+        counter.textContent = Number(counter.textContent) + 1;
     })
     reset.addEventListener("click",() => {
-        counter.value = 0;
+        counter.textContent = 0;
     })
 };
